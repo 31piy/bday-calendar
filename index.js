7024c969bdb70bcd35152b08329f7c5d932afbc8
Piyush Sonagara <piyush@walkover.in>
2017-10-10 21:09:17 +0530

    docs: Update documentation comments

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const updateButton = document.querySelector('#update-button');
 
+// Register a click event on the update button
 updateButton.addEventListener('click', () => {
   const data = document.querySelector('#json-input').value;
   const year = document.querySelector('#year-input').value;
 
+  // Parse the data and validate
   if (Boolean(data) && Boolean(year)) {
     const json = JSON.parse(data);
 
     const relevant = json
+      // Filter the data based on year
       .filter(item => item.birthday.split('/')[2] === year)
+      // Convert the date strings to date objects
       .map(item => {
         item.birthday = new Date(item.birthday);
         return item;
       });
 
+    // Get groups of days
     const groups = getDayGroups(relevant);
+
+    // Render the results
     render(groups);
   }
 });
 
+/**
+ * Groups the collection of members by their birth-days. Also sorts each collection by the
+ * birthday. Returns a key-value object, whose key is birth-day and value is an array of
+ * members.
+ * @param {*} data The collection of members
+ */
 function getDayGroups(data) {
+  // Create groups of birth-days
   const dayGroup = data.reduce((memo, item) => {
     const day = item.birthday.getDay();
 
@@ -31,6 +45,7 @@ function getDayGroups(data) {
     return memo;
   }, {});
 
+  // Sort each group based on birthdays
   for (day in dayGroup) {
     dayGroup[day] = dayGroup[day].sort((a, b) => a.birthday.getTime() - b.birthday.getTime());
   }
@@ -38,14 +53,23 @@ function getDayGroups(data) {
   return dayGroup;
 }
 
+/**
+ * Renders the grouped collection of members on the UI. It internally prepares the HTML
+ * by calculating the number of rows required.
+ * @param {*} groups The grouped collection of members.
+ */
 function render(groups) {
+  // Clear out current data in HTML
   clearOldData();
 
   for (day in groups) {
     const birthdays = groups[day];
+
+    // Calculate the least scale required for the grid
     const scale = getMatrixScale(birthdays.length);
+
+    // It will hold the HTML
     let html = [`<div class="members-row">`];
-    console.log(birthdays);
 
     for (let i = 0; i < scale * scale; i++) {
       if (i > birthdays.length - 1) {
@@ -64,16 +88,25 @@ function render(groups) {
     }
 
     html.push(`</div>`);
+
+    // Assign the HTML to the UI
     document.querySelector(`.weekday.weekday-${day} .members`).innerHTML = html.join('');
   }
 }
 
+/**
+ * Utility function to clear the old HTML of members
+ */
 function clearOldData() {
   for (let i = 0; i < 7; i++) {
     document.querySelector(`.weekday.weekday-${i} .members`).innerHTML = '';
   }
 }
 
+/**
+ * Returns the least scale of the matrix, which can contain `length` elements.
+ * @param {*} length The number of elements to contain.
+ */
 function getMatrixScale(length) {
   if (length === 1) {
     return 1;
@@ -88,10 +121,18 @@ function getMatrixScale(length) {
   return i;
 }
 
+/**
+ * Returns the initials of a name. Requires that first name and last name to be
+ * concatenated by a single space.
+ * @param {*} name The name from which the initials are drawn
+ */
 function getInitials(name) {
   return name.split(' ').map(item => item[0]).join('');
 }
 
+/**
+ * Generates a random hash for a dark-color.
+ */
 function getRandomRolor() {
   var letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var color = '#';
