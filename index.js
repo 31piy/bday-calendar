578056e112f81dee8dc057d1bafde26ee6d9e55e
Piyush Sonagara <piyush@walkover.in>
2017-10-10 19:41:15 +0530

    fix: Implement logic for calculating and rendering birthdays

--- index.js
+++ index.js
@@ -6,11 +6,87 @@ updateButton.addEventListener('click', () => {
 
   if (Boolean(data) && Boolean(year)) {
     const json = JSON.parse(data);
+
     const relevant = json
       .filter(item => item.birthday.split('/')[2] === year)
       .map(item => {
         item.birthday = new Date(item.birthday);
         return item;
       });
+
+    const groups = getDayGroups(relevant);
+    render(groups);
   }
 });
+
+function getDayGroups(data) {
+  const dayGroup = data.reduce((memo, item) => {
+    const day = item.birthday.getDay();
+
+    if (!memo[day]) {
+      memo[day] = [];
+    }
+
+    memo[day].push(item);
+    return memo;
+  }, {});
+
+  for (day in dayGroup) {
+    dayGroup[day] = dayGroup[day].sort((a, b) => a.birthday.getTime() - b.birthday.getTime());
+  }
+
+  return dayGroup;
+}
+
+function render(groups) {
+  clearOldData();
+
+  for (day in groups) {
+    const birthdays = groups[day];
+    const scale = getMatrixScale(birthdays.length);
+    let html = [];
+    console.log(birthdays);
+
+    for (let i = 0; i < scale; i++) {
+      html.push(`<div class="members-row">`);
+
+      for (let j = 0; j < scale; j++) {
+        const member = birthdays[i + j];
+
+        if (!member) {
+          html.push(`<div class="member empty"></div>`);
+        } else {
+          html.push(`<div class="member">${getInitials(member.name)}</div>`)
+        }
+      }
+
+      html.push(`</div>`);
+    }
+
+    document.querySelector(`.weekday.weekday-${day} .members`).innerHTML = html.join('');
+  }
+}
+
+function clearOldData() {
+  for (let i = 0; i < 7; i++) {
+    document.querySelector(`.weekday.weekday-${i} .members`).innerHTML = '';
+  }
+}
+
+function getMatrixScale(length) {
+  if (length === 1) {
+    return 1;
+  }
+
+  let i = 2;
+
+  while (length > i * i ) {
+    i++;
+  }
+
+  return i;
+}
+
+function getInitials(name) {
+  return name.split(' ').map(item => item[0]).join('');
+}
